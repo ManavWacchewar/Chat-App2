a0d7308f1db226cb91635b99b6daae77c06f736b
ManavWacchewar <manav.wacchewar@gmail.com>
2023-03-13 19:02:19 +0530

    commit 3

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Navbar from "./Navbar"
 import Search from "./Search"
 import Chats from "./Chats"
-
 const Sidebar = () => {
   return (
     <div className="sidebar">
